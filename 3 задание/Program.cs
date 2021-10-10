6922d203535faaff7c3d96b3244753ad851c6d75
Kristina <kristinka.antipova94@gmail.com>
2021-10-11 00:24:21 +0500

    3 задание

---
+++ 3 задание/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _3_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Hallo!";
+            s = Console.ReadLine();
+            char[] sReverse = s.ToCharArray();
+            Array.Reverse(sReverse);
+            s = new string(sReverse);
+            Console.WriteLine(s);
+            Console.ReadLine();
+
+        }
+    }
+}

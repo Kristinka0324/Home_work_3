6a9fb47bd06b97eed5749c95317b1957a5216d60
Kristina <kristinka.antipova94@gmail.com>
2021-10-11 00:03:01 +0500

    1. Написать программу, выводящую элементы двумерного массива по диагонали.

---
+++ 1 задание/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace _1_задание
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int next_string = 0;
+            int[,] array = new int[5, 5];
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    array[i, j] = next_string + 1;
+                    Console.WriteLine($"{new string(' ', next_string)}{array[i, j]}");
+                    next_string++;
+                }
+            }
+
+        }
+    }
+}
